6abd8e5a77a2cb17953ca253fd6a545e7dd03f28
Taishuk Emam <157725323+Taishuk0921@users.noreply.github.com>
2025-08-26 11:21:44 +0600

    Update About.java

--- src/university/management/system/About.java
+++ src/university/management/system/About.java
@@ -27,12 +27,12 @@ public class About extends JFrame {
         name.setFont(new Font("Tahoma", Font.BOLD, 30));
         add(name);
         
-        JLabel rollno = new JLabel("Roll number: 1533146");
+        JLabel rollno = new JLabel("Roll number: 23201649");
         rollno.setBounds(70, 280, 550, 40);
         rollno.setFont(new Font("Tahoma", Font.PLAIN, 30));
         add(rollno);
         
-        JLabel contact = new JLabel("Contact: codeforinterview03@gmail.com");
+        JLabel contact = new JLabel("Contact: taishuk0921@gmail.com");
         contact.setBounds(70, 340, 550, 40);
         contact.setFont(new Font("Tahoma", Font.PLAIN, 20));
         add(contact);
